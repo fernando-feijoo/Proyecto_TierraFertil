ac45bd158c4505518daace6c5b8afd993c6a8661
Fernando Temporal <fernando95f4+1@gmail.com>
2022-09-10 19:18:48 -0500

    Primer cambio de prueba sin ser colaborador

--- Proyecto_TierraFertil/src/Main/Main_Proyecto.java
+++ Proyecto_TierraFertil/src/Main/Main_Proyecto.java
@@ -2,6 +2,7 @@ package Main;
 public class Main_Proyecto {
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba para trabajo en grupo");
     }
     
 }

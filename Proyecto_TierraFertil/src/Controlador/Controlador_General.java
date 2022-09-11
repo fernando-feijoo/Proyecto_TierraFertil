69cd0d192a5a41fa1bce2d9a91041635fc8d93ce
PC ESCRITORIO <fernando95f4@gmail.com>
2022-09-11 16:15:05 -0500

    Actualizacion de VistaGeneral y formatos a usar

--- Proyecto_TierraFertil/src/Controlador/Controlador_General.java
+++ Proyecto_TierraFertil/src/Controlador/Controlador_General.java
@@ -3,11 +3,10 @@ package Controlador;
 import Vista.Vista_General;
 
 public class Controlador_General {
+    
     Vista_General vistaGeneral;
 
     public Controlador_General(Vista_General vistaGeneral) {
         this.vistaGeneral = vistaGeneral;
-        
     }
-    
 }

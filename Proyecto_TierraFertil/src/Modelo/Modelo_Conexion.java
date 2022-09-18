b2133a48582b26c82e70d7c0ddbcd3fcc943e766
Chuchei <chuchei5243@gmail.com>
2022-09-18 10:57:27 -0500

    actualizacion modelo conexion

--- Proyecto_TierraFertil/src/Modelo/Modelo_Conexion.java
+++ Proyecto_TierraFertil/src/Modelo/Modelo_Conexion.java
@@ -11,7 +11,7 @@ public class Modelo_Conexion {
     Connection conexion;
      public Connection conectarBD() {
         try {
-            String url = "jdbc:postgresql://localhost:5432/tierrafertil";
+            String url = "jdbc:postgresql://localhost:5432/bd_tierra_fertil";
             String user = "postgres";
             String pass = "postgres";
 

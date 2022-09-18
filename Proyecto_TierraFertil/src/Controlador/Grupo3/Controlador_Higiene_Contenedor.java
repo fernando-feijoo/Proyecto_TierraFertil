9b7c9747acf4379003324653c265e78532670eff
PC PORTATIL <fernando95f4@gmail.com>
2022-09-18 14:09:47 -0500

    Actualizacion desde la Portatil *Fernando

--- Proyecto_TierraFertil/src/Controlador/Grupo3/Controlador_Higiene_Contenedor.java
+++ Proyecto_TierraFertil/src/Controlador/Grupo3/Controlador_Higiene_Contenedor.java
@@ -1,21 +1,43 @@
 package Controlador.Grupo3;
 
+import Modelo.Grupo3.Modelo_Contenedores;
 import Modelo.Grupo3.Modelo_Higiene_Contenedor;
 import Vista.Grupo3.Vista_Llegada;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Controlador_Higiene_Contenedor implements MouseListener {
 
     Vista_Llegada vistaLlegada;
     Modelo_Higiene_Contenedor modeloHigCont = new Modelo_Higiene_Contenedor();
+    Modelo_Contenedores modeloHeContenedor = new Modelo_Contenedores();
     public static int idContenedor;
     String opcionUno, opcionDos, opcionTres, opcionCuatro, opcionCinco, opcionSeis,
             opcionSiete, opcionOcho, observacionHigCont;
+    ResultSet rs;
+    public static int idHigCont;
 
     public Controlador_Higiene_Contenedor(Vista_Llegada vistaLlegada) {
         this.vistaLlegada = vistaLlegada;
         this.vistaLlegada.btn_siguiente_higCont.addMouseListener(this);
+        // Hay que modificar esto donde ira.
+        this.idEntidadHigCont();
+    }
+
+    //  Se consultaria luego de guardar o querer hacer una nueva insercion(actualizacion)
+    //  Lo de la actualizacion toca revisar porque puede haber inconvenientes, toca trajar la logica de idMAX
+    public void idEntidadHigCont() {
+        try {
+            rs = modeloHigCont.consultaID_entidadHigCont();
+            while (rs.next()) {
+                idHigCont = Integer.parseInt(rs.getString("id_tablaHigCont"));
+            }
+            System.out.println("id Entidad Contenedor ControladorDatosLLegada: " + idHigCont);
+        } catch (SQLException ex) {
+            System.out.println("Error de consulta y almacenamiento: " + ex);
+        }
     }
 
     public void autoIncrementarID_Entidades(int herenciaTres) {
@@ -36,38 +58,51 @@ public class Controlador_Higiene_Contenedor implements MouseListener {
     }
 
     public void enviarHigieneContenedor() {
+        int maxDato;
+        maxDato = (idHigCont-8);
+        this.modeloHeContenedor.obser_Hig_Cont = observacionHigCont;
+        this.modeloHeContenedor.guardarContenedorDatos();
+        System.out.println("idC: " + this.modeloHeContenedor.id + " , " + this.modeloHeContenedor.codigo_Contenedor);
         for (int i = 1; i < 9; i++) {
             this.modeloHigCont.id_contenedor = this.idContenedor;
             switch (i) {
                 case 1:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionUno;
                     break;
                 case 2:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionDos;
                     break;
                 case 3:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionTres;
                     break;
                 case 4:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionCuatro;
                     break;
                 case 5:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionCinco;
                     break;
                 case 6:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionSeis;
                     break;
                 case 7:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionSiete;
                     break;
                 case 8:
+                    this.modeloHigCont.id = maxDato+i;
                     this.modeloHigCont.id_limpieza_contenedor = i;
                     this.modeloHigCont.opcion = this.opcionOcho;
                     break;
--- Proyecto_TierraFertil/src/Modelo/Grupo3/Modelo_Higiene_Contenedor.java
+++ Proyecto_TierraFertil/src/Modelo/Grupo3/Modelo_Higiene_Contenedor.java
@@ -13,30 +13,31 @@ public class Modelo_Higiene_Contenedor {
         No trabajaremos con el id de la entidad aqui, ya que se actualiaria donde el ID conendor sea igual
         al consultado. Ademas que eso debe heredar de la consulta contenedor o el dato a actualizar.
     */
-    public int id_contenedor, id_limpieza_contenedor;
+    public int id, id_contenedor, id_limpieza_contenedor;
     public String opcion;
 
     public void guardarActualizar_DatosLlegada() {
         try {
             st = conexion.conectarBD().createStatement();
 
-            String sql = "SELECT \"insertarDatosHigiene\"(id_contenedor, id_limpieza_contenedor, opcion);";
+            String sql = "SELECT \"insertarDatosHigiene\"("+id+", "+id_contenedor+", "+id_limpieza_contenedor+", '"+opcion+"');";
             st.executeUpdate(sql);
             st.close();
-            System.out.println(id_contenedor + " <--ID Conenedor Datos almacenados HiegieneContenedor BD con exito.");
+            System.out.println(id + "<--idTabla \n" + id_contenedor + " <--idCon Conenedor Datos almacenados HiegieneContenedor BD con exito.");
         } catch (Exception e) {
             System.out.println("Error al tratar de guardar datos entidad HigieneContenedor: " + e);
         }
     }
 
     //  NO HARIAMOS CONSULTA DE ID EN ESTA ENTIDAD PORQUE ES TIPO LISTA, SERIA CON WHERE ID = IDCONTENEDOR (QUE ES UNICO)
-    public ResultSet consultaID_entidadDatosLlegada() {
+    //  ## Lo necesitamos para sacar el ultimo id y hacer el registro nuevo.
+    public ResultSet consultaID_entidadHigCont() {
         try {
             st = conexion.conectarBD().createStatement();
-            String sql = "SELECT COALESCE(MAX(id), 0) AS \"id_tablaDatosLlegada\" FROM datos_llegada;";
+            String sql = "SELECT COALESCE(MAX(id), 8) AS \"id_tablaHigCont\" FROM higiene_contenedor;";
             rs = st.executeQuery(sql);
             st.close();
-//            System.out.println("Consulta id entidad Llegada... BD Modelo, " + id);
+            System.out.println("Consulta id entidad HigCont... BD Modelo, " + id);
         } catch (Exception e) {
             System.out.println("Error al tratar de obtener id entidad HigieneContenedor: " + e);
         }
@@ -44,6 +45,6 @@ public class Modelo_Higiene_Contenedor {
     }
     
     public void pruebaGuardado(){
-        System.out.println("HC: " + id_contenedor + " , " + id_limpieza_contenedor + " , " + opcion);
+        System.out.println("HC: " + id + " , " + id_contenedor + " , " + id_limpieza_contenedor + " , " + opcion);
     }
 }

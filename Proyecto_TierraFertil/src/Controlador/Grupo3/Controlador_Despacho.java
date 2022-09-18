6717a96d3eee6b5129e12f9706af84d553179d8e
Chuchei <chuchei5243@gmail.com>
2022-09-17 19:58:41 -0500

    Actualizacion local despacho

--- Proyecto_TierraFertil/src/Controlador/Grupo3/Controlador_Despacho.java
+++ Proyecto_TierraFertil/src/Controlador/Grupo3/Controlador_Despacho.java
@@ -1,5 +1,7 @@
 package Controlador.Grupo3;
 
+import static Controlador.Grupo3.Controlador_Datos_Llegada.idContenedor;
+import static Controlador.Grupo3.Controlador_Datos_Llegada.idDatosLlegada;
 import Modelo.Grupo3.Modelo_Contenedores;
 import Modelo.Grupo3.Modelo_Despacho;
 import Vista.Grupo3.Vista_Llegada;
@@ -9,17 +11,27 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Controlador_Despacho implements MouseListener, ComponentListener {
 
     Vista_Llegada vistaLlegada;
     Modelo_Despacho modeloDespacho = new Modelo_Despacho();
     Modelo_Contenedores modeloContenedor = new Modelo_Contenedores();
+    // SimpleDateFormat formatoP = new SimpleDateFormat("dd/MM/yyyy");
+// this.vistaLlegada.datosLlegada_fechaInsp.setDate(formatoP.format(28/09/2022).toDate);
+    SimpleDateFormat formatoD = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat formatoH = new SimpleDateFormat("HH:mm:ss");
 
     ResultSet rs;
     public static int idContenedor;
     public static int idDespacho;
+    String filtro, termografo_seleccion, termografo, selloAdhesivo, selloVerificador, selloCandado, fechaSalida, horaSalida, selloCable,
+            compañiaTransportista, selloNaviero, vapor, destino, nombrePaletizadores, totalViajar, cajas, cantidadPallet;
 
     public Controlador_Despacho(Vista_Llegada vistaLlegada) {
         this.vistaLlegada = vistaLlegada;
@@ -88,31 +100,30 @@ public class Controlador_Despacho implements MouseListener, ComponentListener {
 
     }
 
-    public void guardarDespacho() {
-// SimpleDateFormat formatoP = new SimpleDateFormat("dd/MM/yyyy");
-// this.vistaLlegada.datosLlegada_fechaInsp.setDate(formatoP.format(28/09/2022).toDate);
-        SimpleDateFormat formatoD = new SimpleDateFormat("dd/MM/yyyy");
-        SimpleDateFormat formatoH = new SimpleDateFormat("HH:mm:ss");
-        String filtro, termografo_seleccion, termografo, selloAdhesivo, selloVerificador, selloCandado, fechaSalida, horaSalida, selloCable,
-                compañiaTransportista, selloNaviero, vapor, destino, nombrePaletizadores, totalViajar, cajas, cantidadPallet;
+    public void almacenarDatosDespacho() {
 
-        filtro = ObtenerFiltro();
-        termografo_seleccion = ObtenerTermografo();
-        termografo = this.vistaLlegada.despacho_termografo.getText();
-        selloAdhesivo = this.vistaLlegada.despacho_sello_adhesivo.getText();
-        selloVerificador = this.vistaLlegada.despacho_sello_verificador.getText();
-        selloCandado = this.vistaLlegada.despacho_sello_exportador.getText();
-        fechaSalida = (String) formatoD.format(this.vistaLlegada.despacho_fechaSalida.getDate());
-        horaSalida = (String) formatoH.format(this.vistaLlegada.despacho_horaSalida);
-        selloCable = this.vistaLlegada.despacho_sello_cable.getText();
-        compañiaTransportista = this.vistaLlegada.despacho_compañia_tansportista.getText();
-        selloNaviero = this.vistaLlegada.despacho_sello_naviero.getText();
-        vapor = this.vistaLlegada.despacho_vapor.getText();
-        destino = this.vistaLlegada.despacho_destino.getText();
-        nombrePaletizadores = this.vistaLlegada.despacho_nombre_paletizadores.getText();
-        totalViajar = this.vistaLlegada.despacho_total_viajar.getText();
-        cajas = this.vistaLlegada.despacho_cajas_total.getText();
-        cantidadPallet = this.vistaLlegada.despacho_cajas_total.getText();
+        try {
+            filtro = ObtenerFiltro();
+            termografo_seleccion = ObtenerTermografo();
+            termografo = this.vistaLlegada.despacho_termografo.getText();
+            selloAdhesivo = this.vistaLlegada.despacho_sello_adhesivo.getText();
+            selloVerificador = this.vistaLlegada.despacho_sello_verificador.getText();
+            selloCandado = this.vistaLlegada.despacho_sello_exportador.getText();
+            fechaSalida = (String) formatoD.format(this.vistaLlegada.despacho_fechaSalida.getDate());
+            horaSalida = (String) formatoH.format(this.vistaLlegada.despacho_horaSalida);
+            selloCable = this.vistaLlegada.despacho_sello_cable.getText();
+            compañiaTransportista = this.vistaLlegada.despacho_compañia_tansportista.getText();
+            selloNaviero = this.vistaLlegada.despacho_sello_naviero.getText();
+            vapor = this.vistaLlegada.despacho_vapor.getText();
+            destino = this.vistaLlegada.despacho_destino.getText();
+            nombrePaletizadores = this.vistaLlegada.despacho_nombre_paletizadores.getText();
+            totalViajar = this.vistaLlegada.despacho_total_viajar.getText();
+            cajas = this.vistaLlegada.despacho_cajas_total.getText();
+            cantidadPallet = this.vistaLlegada.despacho_cajas_total.getText();
+        } catch (Exception e) {
+            System.out.println("Error faltan campos fecha u hora: " + e);
+
+        }
 
         System.out.println(horaSalida);
         System.out.println(fechaSalida);
@@ -121,10 +132,58 @@ public class Controlador_Despacho implements MouseListener, ComponentListener {
 
     }
 
+    public void enviarDatosDespacho() {
+        this.modeloDespacho.id = idDespacho;
+        this.modeloDespacho.id_contenedor = idContenedor;
+        this.modeloDespacho.filtro = filtro;
+        this.modeloDespacho.termografo = termografo_seleccion;
+        this.modeloDespacho.termografo_numero = termografo;
+        this.modeloDespacho.sello_adhesivo = selloAdhesivo;
+        this.modeloDespacho.sello_verificador = selloVerificador;
+        this.modeloDespacho.sello_candado = selloCandado;
+        this.modeloDespacho.fecha_salidad = fechaSalida;
+        this.modeloDespacho.hora_salida = horaSalida;
+        this.modeloDespacho.sello_cable = selloCable;
+        this.modeloDespacho.compañia_transportista = compañiaTransportista;
+        this.modeloDespacho.sello_naviero = selloNaviero;
+        this.modeloDespacho.vapor = vapor;
+        this.modeloDespacho.destino = destino;
+        this.modeloDespacho.nombre_paletizadores = nombrePaletizadores;
+        this.modeloDespacho.total_viajar = totalViajar;
+        this.modeloDespacho.cajas = cajas;
+        this.modeloDespacho.cantidad_pallets = cantidadPallet;
+    }
+
+    public void guardarContenedor() {
+        this.modeloContenedor.id = idContenedor;
+    }
+
+    public void cargarDatosLlegada() {
+        //  Con este codigo solucionamos el colocar la fecha en el campo fecha de libreria jcalendar 1.4 y tambien la hora.
+        Date datoSQLFecha;
+        Date datosHora;
+        try {
+            datoSQLFecha = formatoD.parse("28/09/1995");
+            this.vistaLlegada.datosLlegada_fechaInsp.setDate(datoSQLFecha);
+
+            datosHora = formatoH.parse("05:10:59");
+            this.vistaLlegada.datosLlegada_horaLlegada.setValue(datosHora);
+        } catch (ParseException ex) {
+            Logger.getLogger(Controlador_Datos_Llegada.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     @Override
     public void mouseClicked(MouseEvent me) {
+        // usamos para hacer el cambio de formulario en boton siguiente
+
         if (me.getSource() == this.vistaLlegada.btn_siguiente_despacho) {
             this.vistaLlegada.jp_grupoOpciones_datosLlegada.setSelectedIndex(4);
+            this.almacenarDatosDespacho();
+            this.enviarDatosDespacho();
+            this.guardarContenedor();
+            this.modeloContenedor.guardarContenedorDatos();
+            this.modeloDespacho.guardar_Despacho();
         }
     }
 
@@ -160,7 +219,13 @@ public class Controlador_Despacho implements MouseListener, ComponentListener {
     public void componentHidden(ComponentEvent ce) {
         if (ce.getSource() == this.vistaLlegada.jp_grupoOpciones_datosLlegada) {
             System.err.println("Ingreso Opcion. HIDE");
-            this.guardarDespacho();
+            this.almacenarDatosDespacho();
+            this.enviarDatosDespacho();
+            this.guardarContenedor();
+            this.modeloContenedor.pruebaGuardado();
+
+            System.out.println("ValorSiguiente: " + idContenedor + " , " + idDatosLlegada);
+
         }
     }
 
--- Proyecto_TierraFertil/src/Modelo/Grupo3/Modelo_Despacho.java
+++ Proyecto_TierraFertil/src/Modelo/Grupo3/Modelo_Despacho.java
@@ -4,27 +4,48 @@ import Modelo.Modelo_Conexion;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
 public class Modelo_Despacho {
-    
-    
+
     Modelo_Conexion conexion = new Modelo_Conexion();
     Modelo_Contenedores modeloContenedor = new Modelo_Contenedores();
     ResultSet rs;
     Statement st;
     public int id, id_contenedor;
-    
+    public String filtro, termografo, termografo_numero, sello_adhesivo, sello_verificador, sello_candado, fecha_salidad, hora_salida, sello_cable, compañia_transportista, sello_naviero,
+            vapor, destino, nombre_paletizadores, total_viajar, cajas, cantidad_pallets;
+
+    public boolean guardar_Despacho() {
+        try {
+            st = conexion.conectarBD().createStatement();
+
+            String sql = "SELECT \"insertarDespacho\"(" + id + ", " + id_contenedor + ",'" + filtro + "','" + termografo + "','" + termografo_numero + "','" + sello_adhesivo + "','" + sello_verificador + "','" + sello_candado + ",'" + (fecha_salidad + "" + hora_salida) + "','" + sello_cable + "', "
+                    + "'" + compañia_transportista + "','" + sello_naviero + "','" + vapor + "','" + destino + "','" + nombre_paletizadores + "','" + total_viajar + "','" + cajas + "','" + cantidad_pallets + "')";
+
+            st.executeUpdate(sql);
+            st.close();
+            System.out.println(id + " Datos almacenados Despacho BD con exito.");
+        } catch (Exception e) {
+            System.out.println("Error al tratar guardar los datos de llegada del contenedor " + e);
+        }
+        return true;
+    }
+
     public ResultSet consultaID_entidadDespacho() {
         try {
-            System.out.println("Consulta id entidad Despacho... BD Modelo, " + this.id);
+
             st = conexion.conectarBD().createStatement();
-            String sql = "SELECT COALESCE(MAX(id), 0) AS \"id_tablaDatosDespacho\" FROM datos_despacho;";
+            String sql = "SELECT COALESCE(MAX(id), 1) AS \"id_tablaDatosDespacho\" FROM datos_despacho;";
             rs = st.executeQuery(sql);
             st.close();
+            System.out.println("Consulta id entidad Despacho... BD Modelo, " + this.id);
         } catch (Exception e) {
-            System.out.println("Error al tratar de obtener id entidad datos_despacho: " +e);
+            System.out.println("Error al tratar de obtener id entidad datos_despacho: " + e);
         }
         return rs;
     }
-    
+
+    public void pruebaGuardado() {
+        // para validar si esta enviando bien la informacion.
+         System.out.println( id + "," + id_contenedor + "," + termografo + "," + filtro );
+    }
 }

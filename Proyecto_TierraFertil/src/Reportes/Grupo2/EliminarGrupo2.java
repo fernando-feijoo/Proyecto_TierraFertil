8e0a008ae18e83fb6152e1f6f4d8e21a1f5bd979
PC PORTATIL <fernando95f4@gmail.com>
2022-09-22 22:06:22 -0500

    Actualizacion de proyecto para reportes.

---
+++ Proyecto_TierraFertil/src/Reportes/Grupo2/EliminarGrupo2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reportes.Grupo2;
+
+/**
+ *
+ * @author ANONYMOUS
+ */
+public class EliminarGrupo2 {
+    //  ##Eliminar o modificar##
+}
---
+++ Proyecto_TierraFertil/src/Reportes/Grupo3/EliminarGrupo3.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reportes.Grupo3;
+
+/**
+ *
+ * @author ANONYMOUS
+ */
+public class EliminarGrupo3 {
+    //  ##Eliminar o modificar##
+}

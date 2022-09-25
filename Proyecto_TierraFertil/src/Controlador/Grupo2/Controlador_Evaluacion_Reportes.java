32ed94bc91d19e2aa65811f3903e0e1a89d3e793
Daniel Bravo <danielbravoist@gmail.com>
2022-09-25 13:18:32 -0500

    Actualizacion Previa 2

--- Proyecto_TierraFertil/src/Controlador/Grupo2/Controlador_Evaluacion_Reportes.java
+++ Proyecto_TierraFertil/src/Controlador/Grupo2/Controlador_Evaluacion_Reportes.java
@@ -1,13 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controlador.Grupo2;
+import Modelo.Grupo2.Modelo_Evaluacion_Reportes;
+import Vista.Grupo2.Vista_Reportes_Menu;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.sql.ResultSet;
+import javax.swing.table.DefaultTableModel;
+public class Controlador_Evaluacion_Reportes implements MouseListener{
+    Vista_Reportes_Menu vistaReportes = new Vista_Reportes_Menu();
+    Modelo_Evaluacion_Reportes modeloReportes = new Modelo_Evaluacion_Reportes();
+    ResultSet rs;
+    public Controlador_Evaluacion_Reportes(Vista_Reportes_Menu vistaReportes_Menu) {
+        this.vistaReportes = vistaReportes;
+        mostrarDatos();
+        this.vistaReportes.rdn_imprimir.addMouseListener(this);
+        
+        
+       
+    }
 
-/**
- *
- * @author Daniel Bravo
- */
-public class Controlador_Evaluacion_Reportes {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+     public void mostrarDatos() {
+        DefaultTableModel tabla = (DefaultTableModel) this.vistaReportes.tabla_listado.getModel();
+        tabla.setColumnCount(0);
+        tabla.addColumn("Codigo");
+        tabla.addColumn("Fecha");
+        tabla.addColumn("Semana");
+        tabla.addColumn("Destino");
+        tabla.addColumn("Cajas inspeccionadas");
+        tabla.setRowCount(0);
+
+        try {
+            rs = modeloReportes.mostrarListado();
+            String[] tabulacion = new String[5];
+
+            while (rs.next()) {
+
+                tabulacion[0] = rs.getString("codigo");
+                tabulacion[1] = rs.getString("fecha");
+                tabulacion[2] = rs.getString("id_semana");
+                tabulacion[3] = rs.getString("destino");
+                tabulacion[4] = rs.getString("cajas_inspeccionadas");
+                tabla.addRow(tabulacion);
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error en mostrarListado. " + e);
+        }
+
+    }
+    
+    
     
 }
--- Proyecto_TierraFertil/src/Modelo/Grupo2/Modelo_Evaluacion_Reportes.java
+++ Proyecto_TierraFertil/src/Modelo/Grupo2/Modelo_Evaluacion_Reportes.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo.Grupo2;
-
-/**
- *
- * @author Daniel Bravo
- */
+import Modelo.Modelo_Conexion;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 public class Modelo_Evaluacion_Reportes {
+    Modelo_Conexion conexion = new Modelo_Conexion();
+    ResultSet rs;
+    Statement st;
     
+    public ResultSet mostrarListado() {
+        try {
+            st = conexion.conectarBD().createStatement();
+            String sql = "select distinct  eva.codigo, eva.fecha, eva.id_semana,eva.destino,count(det.caja_inspeccionada)"
+                    + "as cajas_inspeccionadas from evaluaciones_empacadora eva inner join detalle_evaluacion_emp det on"
+                    + "(eva.id=det.id_evaluacion) where eva.estado_act_inac='ACTIVO' group by eva.codigo, eva.fecha, eva.id_semana,eva.destino;";
+
+            rs = st.executeQuery(sql);
+
+            return rs;
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error en mostrarListado en ModeloListado" + e);
+        }
+
+        return null;
+
+    }
 }
